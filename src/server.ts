f8c126780f52926665d2b30376fde81013ef76c9
NadiaPeeya <nadiapeeya05@gmail.com>
2023-09-14 14:12:15 +0900

    setup express, mongoose,cors , parser

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import app from './app'
+import config from "./config";
+async function database() {
+     
+    try {
+        await mongoose.connect(config.database_url as string);
+        console.log("database is connected successfully")
+
+        app.listen(config.port, () => {
+            console.log(`Application app listening on port ${config.port}`)
+          })
+    }
+    catch (err) {
+        console.log("failed to connect database", err)
+    }
+
+  }
+
+
+  database();
